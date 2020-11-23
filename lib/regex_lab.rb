b11541d34d77a1c830a0e0f6060ca91f623555e4
Blaire <grant.blaire@gmail.com>
2020-11-23 14:24:39 -0600

    commit tree 504443525e486b8d274b26aa9aa3a401fa28cc52

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,33 @@
-def starts_with_a_vowel?(word)
+#require 'pry'
 
+def starts_with_a_vowel?(word)
+    if word.match(/\A[aeiouAEIOU]\w/)
+        return true
+    else
+        return false
+    end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/[un|Un]\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/\b[a-zA-Z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    if text.match(/\A[A-Z].+[[:punct:]]\z/)
+        return true
+    else
+        return false
+    end
 end
 
 def valid_phone_number?(phone)
-
+    if phone.match(/\D*\d{3}\D*\d{3}\D*\d{4}/)
+        return true
+    else
+        return false
+    end
 end
